a507f80c010980ee202309e849c513ffa52a7a81
om4ikoma <110773705+om4ikoma@users.noreply.github.com>
2022-08-07 20:05:49 +0600

    Create almasov_omurbek_21-1_hw1.py

---
+++ almasov_omurbek_21-1_hw1.py
@@ -0,0 +1,54 @@
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+
+    def voice(self):
+        print(f"{self.name}: Gaf Gaf Gaf!")
+
+dog = Dog("Dog")
+dog.voice()
+
+
+class Cow:
+
+    def __init__(self, name):
+        self.name = name
+
+    def voice(self):
+        print(f"{self.name}: Muu Muu Muu")
+
+cow = Cow("Cow")
+cow.voice()
+
+class Cat:
+
+    def __init__(self, name):
+        self.name = name
+
+    def voice(self):
+        print(f"{self.name}: Mya Mya May")
+
+cat = Cat("Cat")
+cat.voice()
+
+class Bear:
+
+    def __init__(self, name):
+        self.name = name
+
+    def voice(self):
+        print(f"{self.name}: GRRRRRRRRRRR")
+
+
+bear = Bear("Bear")
+bear.voice()
+
+
+
+
+
+
+
+
+
